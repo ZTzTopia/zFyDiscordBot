e99154de86a0794f0d6091a3e3273d59d677a5f6
ZTzTopiaa <36158219+ZTzTopiaa@users.noreply.github.com>
2018-05-04 11:47:40 +0000

    Fix Website!

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, msg, args) => {
     hep.setColor(msg.guild.me.displayColor)
     .setTitle('zFy\'s help commands in a website!')
     .setThumbnail(bot.user.avatarURL)
-    .addField('Hello there, I\'m zFy\'s Bot! I was created by ZTzTopia®.', '\nzFy\'s Help commands has been moved to a zFy\'s website. \n***https://ZTzTopiaa.github.io/zFy/cmd/lobby.html***')
+    .addField('Hello there, I\'m zFy\'s Bot! I was created by ZTzTopia®.', '\nzFy\'s Help commands has been moved to a zFy\'s website. \n***https://ZTzTopiaa.github.io/zFy/lobby.html***')
     .setFooter(`${bot.user.username}#${bot.user.discriminator}`, bot.user.avatarURL)
     .setTimestamp();
     
