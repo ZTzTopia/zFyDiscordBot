ec84d180f1501dec9f5fdc1791d7777fd0ed220f
ZTzTopiaa <36158219+ZTzTopiaa@users.noreply.github.com>
2018-05-09 15:22:21 +0000

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,25 @@
+var Discord = require('discord.js');
+
+module.exports.run = async (bot, msg, args) => {
+var member = msg.member;
+if (msg.mentions.users.array()[0])
+    member = msg.guild.members.get(msg.mentions.users.array()[0].id);
+var user = member.user;
+
+var info = new Discord.RichEmbed()
+    .setAuthor(user.username + '#' + user.discriminator, user.avatarURL)
+    .setDescription('User Information')
+    .setColor(member.displayHexColor)
+    .addField('Avatar Link', '[Here](' + user.avatarURL + ')', true)
+
+msg.channel.send({ embed: info });
+}
+
+module.exports.help = {
+    name: "avatar",
+    type: "Util",
+    Description: 'Provides information about a avatar user.',
+    format: "`avatar <mention>`",
+    Example: '`avatar` \n`avatar @ZTzTopia`',
+    require: "None."
+}
